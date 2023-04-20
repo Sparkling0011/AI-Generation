7b50dc1a5352d091fe515febeed5245d244da653
xing <1725256426@qq.com>
2023-04-20 12:59:54 +0800

    monitor config

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
-import SDK from "synthetical-monitor";
+import SDK from "synthetical-monitor/lib";
 
 SDK.init({
   pid: "7b4c872d-bef3-443a-b69c-b433cbbb15b0",
